0c34c05380a862ebde5ba7cbbccd9a42d6aec142
Mark Garrigan <mrkgrrgn@gmail.com>
2025-12-29 16:21:53 -0600

    new org

--- Formula/taken.rb
+++ Formula/taken.rb
@@ -1,7 +1,7 @@
 class Taken < Formula
   desc "Clipboard-to-notebook CLI"
-  homepage "https://github.com/NotesTaken/tkn-cli"
-  url "https://github.com/NotesTaken/tkn-cli/archive/refs/tags/v0.0.8.tar.gz"
+  homepage "https://github.com/plainwork/tkn-cli"
+  url "https://github.com/plainwork/tkn-cli/archive/refs/tags/v0.0.8.tar.gz"
   sha256 "7f9bc2bddb850fe9e16c2d7434b5f27c23c4d580d7bae41f6a731f1e5c0d5487"
   version "0.0.8"
 
